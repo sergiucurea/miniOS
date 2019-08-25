dc2d1b7f8edde518f05ad9d21480d7191c5feb85
Sergiu <sergiucurea@gmail.com>
2019-08-25 03:03:00 +0300

    clear repo

--- soare-surse/cleanup.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-del *.pdb /s  > cleanup.log
-del *.ilk /s  >> cleanup.log
-del int\*.* /s /q  >> cleanup.log
-del *.sdf  >> cleanup.log
-del *.suo /ah  >> cleanup.log
--- soare-surse/soare/main.c
+++
@@ -1,9 +0,0 @@
-#include "boot.h"
-#include "screen.h"
-
-void EntryPoint(void)
-{
-    //__magic();                  // break into BOCHS
-
-	HelloBoot();
-}
